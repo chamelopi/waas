9513269a8dc6bcb8b50d5bf51ff10e045d1e6f69
chamelopi <ben.gorny@web.de>
2021-10-02 10:54:55 +0200

    Add primitive water
    - Just a transparent plane at specific height for now
    - Maybe we can write a custom vertex shader later that simulates small
    waves
    - Ignores lighting for now, too, we might want to generate a normal map
    for this later

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ document.body.appendChild(renderer.domElement);
 
 // TODO: Is specifying paths explicitly maintainable?
 const textures = await loadTextures([
-    "frog.png", "froggo.png", "grass.png", "rock.jpg",
+    "frog.png", "froggo.png", "grass.png", "rock.jpg", "water.png",
     "envmap_miramar/miramar_ft.png",
     "envmap_miramar/miramar_bk.png",
     "envmap_miramar/miramar_up.png",
@@ -48,52 +48,56 @@ frogMaterial.transparent = true;
 const frog3d = models["assets/froggo.glb"];
 frog3d.material = new THREE.MeshBasicMaterial({ map: textures["assets/froggo.png"] });
 // To make it face the camera
-frog3d.rotation.z = -3*Math.PI / 8;
+frog3d.rotation.z = -3 * Math.PI / 8;
 frog3d.scale.set(0.5, 0.5, 0.5);
-frog3d.position.set(2, 1, 0);
+frog3d.position.set(2.5, 1, -0.5);
 
 scene.add(frog3d);
 
 const skybox = makeSkybox(textures, "assets/envmap_miramar", "miramar", "png");
 scene.add(skybox);
 
-const ground = new THREE.Mesh(new THREE.PlaneBufferGeometry(50, 50), new THREE.MeshBasicMaterial({ map: textures["assets/grass.png"] }));
-ground.position.y = -2;
-ground.rotation.x = -Math.PI / 2;
-//scene.add(ground);
+// TODO: Replace with sth that textures better
+const water = new THREE.Mesh(new THREE.PlaneBufferGeometry(20, 20),
+    new THREE.MeshBasicMaterial({ map: textures["assets/water.png"], transparent: true, opacity: 0.65 }));
+water.position.y = 0.6;
+water.rotation.x = -Math.PI / 2;
+scene.add(water);
 
 const geometry = new THREE.BoxGeometry();
 const froggo = new THREE.Mesh(geometry, [frogMaterial, undefined, undefined, undefined, undefined, undefined]);
-froggo.position.set(2, 2, 0);
-froggo.rotation.set(0, -Math.PI/2, 0);
+froggo.position.set(-0.9, 1, -7);
+froggo.rotation.set(0, -Math.PI / 2, 0);
 scene.add(froggo);
 
 const froggo2 = new THREE.Mesh(geometry, [frogMaterial, undefined, undefined, undefined, undefined, undefined]);
-froggo2.position.set(-7, 1, -4);
+froggo2.position.set(-8, 1, -5);
 froggo2.scale.set(2, 2, 2);
-froggo2.rotation.y = -Math.PI/4;
+froggo2.rotation.y = -Math.PI / 4;
 scene.add(froggo2);
 
+// TODO: Third frog jumping
+
 let dir = new THREE.Vector2(0.01, 0.01);
-let vel = 0.03;
+let vel = 0.015;
+let vel2 = 0.02;
 // Main loop
 function animate() {
     // Update
-    if (froggo.position.x > 3 || froggo.position.x < -3) {
-        dir.x = -dir.x;
+    froggo.position.y += vel;
+    if (froggo.position.y > 3.5) {
+        vel -= 0.05;
     }
-    if (froggo.position.y > 2.5 || froggo.position.y < -1.5) {
-        dir.y = -dir.y;
+    if (froggo.position.y <= 1.0) {
+        vel = 0.015;
     }
-    froggo.position.x += dir.x;
-    froggo.position.y += dir.y;
 
-    froggo2.position.y += vel;
-    if (froggo2.position.y > 3) {
-        vel -= 0.1;
+    froggo2.position.y += vel2;
+    if (froggo2.position.y > 3.5) {
+        vel2 -= 0.05;
     }
     if (froggo2.position.y <= 1.0) {
-        vel = 0.03;
+        vel2 = 0.02;
     }
 
     requestAnimationFrame(animate);
@@ -110,5 +114,7 @@ window.addEventListener("resize", () => {
 
 
 // TODOs:
+// - Camera controls (WASD movement, maybe zoom?)
+// - 
 // - Use webpack to reduce distribution size
 //   - Bonus points if the loading screen only shows the site on wednesdays
